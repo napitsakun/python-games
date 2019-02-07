b44365229dc60ae35ccd78a2c5bdce9d5772e025
Sakun <napitsakun@gmail.com>
2019-02-07 09:16:45 +0545

    Play hangman and quiz game.

--- quizGame.py
+++ quizGame.py
@@ -3,53 +3,52 @@
 
 import json
 import random
-#import os
 
 # to deduct any two wrong answers. Parameters to receive question dictionary ani rightAnswer count
-def choice_fifty_fifty(question_dict, rightAnswer):    
-    # to hold key of every option (answer) 
-    key_list = [] 
+def choice_fifty_fifty(question_dict, rightAnswer):
+    # to hold key of every option (answer)
+    key_list = []
     c_ans = question_dict['ca'] # correct answer
-      
+
     for key in question_dict.keys():
-        key_list.append(key) 
+        key_list.append(key)
         if key == c_ans:
-            correctAns = c_ans 
-    
-    # to remove the last key ('ca') that is not needed         
-    key_list.pop(5) 
+            correctAns = c_ans
+
+    # to remove the last key ('ca') that is not needed
+    key_list.pop(5)
     # to remove the first key ('q') that is not needed
-    key_list.pop(0)     
-    
-    # to generate a random option     
-    while True: 
+    key_list.pop(0)
+
+    # to generate a random option
+    while True:
         # choose one of the wrong answer randomly
-        rand_wrong_choice = random.choice(key_list) 
-        
+        rand_wrong_choice = random.choice(key_list)
+
         if rand_wrong_choice == c_ans:
             continue
         else:
             break
-    
+
     # compare the ascii value of keys to display options in correct order
-    if ord(correctAns) < ord(rand_wrong_choice): 
+    if ord(correctAns) < ord(rand_wrong_choice):
         print("\n{}) {}\n{}) {}\n".format(correctAns, question_dict[correctAns], rand_wrong_choice, question_dict[rand_wrong_choice]))
         answer = input("\nEnter your answer here ({}/{}) : ".format(correctAns, rand_wrong_choice))
     else:
         print("\n{}) {}\n{}) {}\n".format(rand_wrong_choice, question_dict[rand_wrong_choice], correctAns, question_dict[correctAns]))
         answer = input("\nEnter your answer here ({}/{}) : ".format(rand_wrong_choice, correctAns))
-    
-    
+
+
     if question_dict['ca'] == answer:
         print("Correct answer.\t +10 points")
         print("------------------------------------------------------")
         rightAnswer += 1
-                        
+
     else:
         print("\nYour answer is wrong.")
         print("Correct answer is '{}'.".format(question_dict['ca']))
         print("------------------------------------------------------")
-    
+
     return rightAnswer
 
 
@@ -57,39 +56,39 @@ with open("quizQues.json", "r") as qa:
     # read json file
     questionSet = qa.read()
     # load content of file in list form
-    questionList = json.loads(questionSet) 
+    questionList = json.loads(questionSet)
     rightAnswer = 0
     player_name = input("Enter your name: ")
-    
+
     print("WELCOME {}, be ready for your questions\n".format(player_name))
-    
-    for i in range(len(questionList)): 
+
+    for i in range(len(questionList)):
         # access the first item of list (first question of quiz)
-        question_dict = questionList[i] 
-        
+        question_dict = questionList[i]
+
         # print question
-        print("\n{questionNo}.  {question}".format(questionNo = i+1, question = question_dict['q'])) 
+        print("\n{questionNo}.  {question}".format(questionNo = i+1, question = question_dict['q']))
         # print options
         print(" a) {}\n b) {}\n c) {}\n d) {}".format(question_dict['a'], question_dict['b'], question_dict['c'], question_dict['d']))
         print("\nLifelines: \n1) 50/50")
-        
-        answer = input("\nEnter your answer here (a/b/c/d) : ")
-        
+
+        answer = input("\nEnter your choice here (a/b/c/d)/(1) : ")
+
         if question_dict['ca'] == answer:
             print("Correct answer.\t +10 points")
             print("------------------------------------------------------")
             rightAnswer += 1
-            
+
         # user asks for 50/50 lifeline
-        elif answer == '1': 
+        elif answer == '1':
             rightAnswer = choice_fifty_fifty(question_dict, rightAnswer)
-            
+
         else:
             print("\nYour answer is wrong.")
             print("Correct answer is '{}'.".format(question_dict['ca']))
             print("------------------------------------------------------")
         #os.system('cls')
-        
+
     else:
         print("\nGame Over!!!")
         print("You made {} right. Your score is {} out of 100\n".format(rightAnswer, str(rightAnswer*10)))
